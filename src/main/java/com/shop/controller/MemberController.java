7bf9ab1b062ace70d9fb9a3bbe6d7cee6a359e31
KwonYH <yhoung11@gmail.com>
2023-06-27 00:10:59 +0900

    Validation

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -2,12 +2,18 @@ package com.shop.controller;
 
 
 import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -15,13 +21,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
 	private final MemberService memberService;
+	private final PasswordEncoder passwordEncoder;
 
 	/**
 	 * 회원 가입 페이지로 이동할 수 있도록 {@link MemberController} 클래스에 메소드를 작성한다.
 	 */
 	@GetMapping(value = "/new")
 	public String memberForm(Model model) {
-		model.addAttribute("memerFormDto", new MemberFormDto());
+		model.addAttribute("memberFormDto", new MemberFormDto());
 		return "member/memberForm";
+	}
+
+	@PostMapping(value = "/new")
+	public String memberForm(MemberFormDto memberFormDto) {
+		Member member = Member.createMember(memberFormDto, passwordEncoder);
+		memberService.saveMember(member);
+
+		return "redirect:/";
+	}
+
+	public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "member/memberForm";
+		}
+
+		try {
+			Member member = Member.createMember(memberFormDto, passwordEncoder);
+			memberService.saveMember(member);
+		} catch (IllegalStateException e) {
+			model.addAttribute("errorMessage", e.getMessage());
+			return "member/memberForm";
+		}
+
+		return "redirect:/";
 	}
 }
