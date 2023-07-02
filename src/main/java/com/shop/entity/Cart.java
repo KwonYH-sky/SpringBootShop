e8d57b3182595261a9e761575face04799f5afa1
KwonYH <yhoung11@gmail.com>
2023-07-03 01:19:48 +0900

    ManyToOne and OneToMany

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -19,7 +19,7 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@OneToOne // @OneToOne 어노테시션을 이용해 외원 엔티티와 일대일 매핑을 한다.
+	@OneToOne // @OneToOne 어노테시션을 이용해 회원 엔티티와 일대일 매핑을 한다.
 	// @JoinColumn 어노테시션을 이용해 매핑할 외래키를 지정한다.
 	// name 속성에는 매핑할 외래키의 이름을 설정한다.
 	// @JoinColumn의 name을 명시하지않으면 JPA가 알아서 ID를 찾지만 컬럼명이 원하는 대로 생성되지 않을 수 있어 직접 지정
