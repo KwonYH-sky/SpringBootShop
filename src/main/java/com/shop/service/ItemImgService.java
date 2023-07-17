ec84c51d82cc2b8272b44138a5e7d414270cc305
KwonYH <yhoung11@gmail.com>
2023-07-18 00:15:29 +0900

    상품 수정 1

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -44,5 +46,31 @@ public class ItemImgService {
 		// imgUrl: 업로드 결과 로컬에 저장된 상품 이미지 파일을 불러오는 경로
 		itemImg.updateItemImg(oriImgName, imgName, imgUrl); 
 		itemImgRepository.save(itemImg);
+	}
+
+	public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
+		if(!itemImgFile.isEmpty()) { // 상품 이미지를 수정한 경우 상품 이미지를 업데이트한다.
+			// 상품 이미지 아이디를 이용하여 기존에 저장했던 상품 이미지 엔티티를 조회한다.
+			ItemImg savedItemImg = itemImgRepository.findById(itemImgId).orElseThrow(EntityNotFoundException::new);
+
+			// 기존 이미지 삭제
+			if(!StringUtils.isEmpty(savedItemImg.getImgName())) { // 기존에 등록된 상품 이미지 파일이 있을 경우 해당 파일을 삭제한다.
+				fileService.deleteFile(itemImgLocation + "/" + savedItemImg.getImgName());
+			}
+
+			String oriImgName = itemImgFile.getOriginalFilename();
+			// 업데이트한 상품 이미지 파일을 업로드한다.
+			String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+			String imgUrl = "/images/item/" + imgName;
+
+			/*
+			 변경된 상품 이미지 정보를 세팅해준다.
+			 여기서 중요한 점은 상품 등록 때처럼 itemImgRepository.save() 로직을 호출하지 않았다는 것이다.
+			 savedItemImg 엔티티는 현재 영속 상태이므로
+			 데이터를 변경하는 것만으로 변경 감지 기능이 동작하여 트랜잭션이 끝날 때 updateItemImg 쿼리가 실행된다.
+			 여기서 중요한 것은 엔티티가 영속 상태이여야 한다는 점이다.
+			*/
+			savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
+		}
 	}
 }
--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.shop.service;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemImgDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,6 +42,27 @@ public class ItemService {
 		}
 
 		return item.getId();
+	}
+
+	// 상품 데이터를 읽어오는 트랜잭션을 읽기 전용으로 설정한다.
+	// 이럴 경우 JPA가 더티체킹(변경감지)을 수행하지 않아서 성능을 향상 시킬 수 있다.
+	@Transactional(readOnly = true)
+	public ItemFormDto getItemDtl(Long itemId) {
+
+		// 해당 상품의 이미지를 조회한다.
+		// 등록순으로 가지고 오기 위해서 상품 이미지 아이디 오름차순으로 가지고 온다.
+		List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+		List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+		for (ItemImg itemImg: itemImgList) { // 조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어서 리스트에 추가한다.
+			ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+			itemImgDtoList.add(itemImgDto);
+		}
+
+		// 상품의 아이디를 통해 상품 엔티티를 조회한다. 존재하지 않을 때는 EntityNotFoundException을 발생시킨다.
+		Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+		ItemFormDto itemFormDto = ItemFormDto.of(item);
+		itemFormDto.setItemImgDtoList(itemImgDtoList);
+		return itemFormDto;
 	}
 
 }
