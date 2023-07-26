2bc9d59c0670d0a20ca6569c2bf76a69edc30ff2
KwonYH <yhoung11@gmail.com>
2023-07-27 00:02:31 +0900

    Order 기능 1

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -43,4 +43,36 @@ public class Order extends BaseEntity {
                 orphanRemoval = true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    /**
+     * orderItems에는 주문 상품 정보들을 담아준다.
+     * orderItem 객체를 order 객체의 orderItems에 추가한다.
+     * Order 엔티티와 OrderItem 엔티티가 양방향 참조 관계 이므로, orderItem 객체에도 order 객체를 세팅한다.
+     */
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member); // 상품을 주문한 회원의 정보를 세팅한다.
+        /*
+        상품 페이지에서는 1개의 상품을 주문하지만, 장바구니 페이지에서는 한 번에 여러 개의 상품을 주문할 수 있다.
+        따라서 여러 개의 주문 상품을 담을 수 있도록 리스트 형태로 파라미터 값을 받으며 주문 객체에 orderItem 객체를 추가한다.
+         */
+        for (OrderItem orderItem: orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER); // 주문 상태를 "ORDER"로 세팅한다.
+        order.setOrderDate(LocalDateTime.now()); // 현재 시간을 주문 시간으로 한다.
+        return order;
+    }
+
+    public int getTotalPrice() { // 총 주문 금액을 구하는 메소드
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -28,4 +28,22 @@ public class OrderItem extends BaseEntity{
 
     private int count; // 수량
 
+    public static OrderItem createOrderItem(Item item, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item); // 주문할 상품과 주문 수량을 세팅한다.
+        orderItem.setCount(count);
+        /*
+        현재 시간 기준으로 상품 가격을 주문 가격으로 세팅한다.
+        상품 가격은 시간에 따라서 달라질 수 있다.
+        또한 쿠폰이나 할인을 적용하는 케이스들도 있지만 여기서는 고려하지 않는다.
+         */
+        orderItem.setOrderPrice(item.getPrice());
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public int getTotalPrice() { // 주문 가격과 주문 수량을 곱해서 해당 상품을 주문한 총 가격을 계산하는 메소드이다.
+        return orderPrice*count;
+    }
 }
