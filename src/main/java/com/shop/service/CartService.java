2aebbd2ee3b8fd9556c8a38d5ca91fdcd11b5368
KwonYH <yhoung11@gmail.com>
2023-08-09 00:36:47 +0900

    장바구니 주문

--- src/main/java/com/shop/service/CartService.java
+++ src/main/java/com/shop/service/CartService.java
@@ -2,6 +2,8 @@ package com.shop.service;
 
 import com.shop.dto.CartDetailDto;
 import com.shop.dto.CartItemDto;
+import com.shop.dto.CartOrderDto;
+import com.shop.dto.OrderDto;
 import com.shop.entity.Cart;
 import com.shop.entity.CartItem;
 import com.shop.entity.Item;
@@ -28,6 +30,7 @@ public class CartService {
 	private final MemberRepository memberRepository;
 	private final CartRepository cartRepository;
 	private final CartItemRepository cartItemRepository;
+	private final OrderService orderService;
 
 	public Long addCart(CartItemDto cartItemDto, String email){
 		// 장바구니에 담은 상품 엔티티를 조회
@@ -103,5 +106,31 @@ public class CartService {
 	public void deleteCartItem(Long cartItemId) {
 		CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
 		cartItemRepository.delete(cartItem);
+	}
+
+	public Long orderCartItem(List<CartOrderDto> cartOrderDtoList, String email) {
+		List<OrderDto> orderDtoList = new ArrayList<>();
+
+		// 장바구니 페이지에서 전달받은 주문 상품 번호를 이용하여 주문 로직으로 전달할 orderDto 객체를 만든다.
+		for (CartOrderDto cartOrderDto : cartOrderDtoList){
+			CartItem cartItem = cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+
+			OrderDto orderDto = new OrderDto();
+			orderDto.setItemId(cartItem.getItem().getId());
+			orderDto.setCount(cartItem.getCount());
+			orderDtoList.add(orderDto);
+		}
+
+		// 장바구니에 담은 상품을 주문하도록 주문 로직을 호출한다.
+		Long orderId = orderService.orders(orderDtoList, email);
+
+		// 주문한 상품들을 장바구니에서 제거한다.
+		for (CartOrderDto cartOrderDto : cartOrderDtoList) {
+			CartItem cartItem = cartItemRepository.findById(cartOrderDto.getCartItemId()).orElseThrow(EntityNotFoundException::new);
+
+			cartItemRepository.delete(cartItem);
+		}
+
+		return orderId;
 	}
 }
